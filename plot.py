c55a3d1a3baa7c008d92aa3470785e94fec1b7ab
Altoidnerd <allenmajs1@gmail.com>
2015-01-13 21:00:16 -0500

    plot a vector

---
+++ plot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+from sys import argv
+script, infile = argv
+
+v = []
+x = []
+k=0
+with open(infile, "r") as f:
+    for line in f:
+        v.append(float(line.strip('\r\n')))
+        x.append(int(k))
+        k+=1
+
+plt.plot(x,v)
+plt.show()
