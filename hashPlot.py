6ca402cf59c146ab3e553dc10bf0d2ed6f3e27be
Altoidnerd <allenmajs1@gmail.com>
2015-05-07 00:57:06 -0400

    Create hashPlot.py

---
+++ hashPlot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#
+# example data for this file is in the file .hash in this repo
+#
+
+import sys
+import os
+from sys import argv
+
+infile = sys.argv[1]
+hashTags = []
+tagFrequency = []
+countList = []
+
+def tagFunc():
+    pass
+def freqFunc():
+    pass
+def countFunc():
+    pass
+
+with open(infile) as f:
+    for line in f:
+        linelist=line.split()
+        if linelist[0]="#":
+            tagFunc(linelist)
+        else:
+            linelist = linelist.replace(",","")
+            if len(linelist) > 3:
+                freqFunc()
+            else:
+                countFunc():
